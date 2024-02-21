81dbff7f4727d78c8114483fea390e0a5a5dce0a
omarse7a <omar.seha90@gmail.com>
2024-02-22 01:50:31 +0200

    added player movement and animations

--- code/player.py
+++ code/player.py
@@ -12,6 +12,23 @@ class Player(pygame.sprite.Sprite):
         self.frames = [spaceship1, spaceship2]
         self.index = 0
         self.image = self.frames[int(self.index)]
-        self.rect = self.image.get_rect(midbottom = (400, 570))
+        self.rect = self.image.get_rect(midbottom = (400, 590))
 
-    
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and self.rect.right < 800:
+            self.rect.x += 5
+        
+        elif (keys[pygame.K_a] or keys[pygame.K_LEFT] )and self.rect.left > 0:
+            self.rect.x -= 5
+
+    def player_animation(self):
+
+        self.index += 0.04
+        if self.index >= len(self.frames): self.index = 0
+        self.image = self.frames[int(self.index)]
+
+    def update(self):
+        
+        self.player_input()
+        self.player_animation()
